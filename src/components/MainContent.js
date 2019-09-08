91fb896a60116dc61aec312de199ec47cde4539d
Gokul <pgokulmca@gmail.com>
2019-09-08 18:45:50 +0530

    TodoItem component added

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import TodoItem from "./TodoItem";
 
 function MainContent() {
     return(
-        <div className="c-main_content">
-            <input type="checkbox" />
-            <p>THis is the first item</p>
-            <input type="checkbox" />
-            <p>THis is the first item</p>
-            <input type="checkbox" />
-            <p>THis is the first item</p>
-            <input type="checkbox" />
-            <p>THis is the first item</p>
+        <div className="c-todo_list">
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
         </div>
     )
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoItem() {
+    return (
+        <div className="c-todo_item">
+            <input type="checkbox" />
+            <p>THis is the first item</p>
+        </div>
+    )
+}
+
+export default TodoItem;
