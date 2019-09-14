5cd6bad245d9d1dc89efeac9227931fd6360b31e
Gokul <pgokulmca@gmail.com>
2019-09-14 23:14:14 +0530

    setState with the checkbox value

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,10 @@ import React from 'react';
 function TodoItem(props) {
     return (
         <div className="c-todo_item" style={{color: props.todo.completed ? 'lightgray' : 'green' }}>
-            <input type="checkbox" checked={props.todo.completed} />
+            <input
+                type="checkbox"
+                checked={props.todo.completed}
+                onChange={(event) => props.handleClickEvent(props.todo.id, event.target.checked)} />
             <p>{props.todo.text}</p>
         </div>
     )
